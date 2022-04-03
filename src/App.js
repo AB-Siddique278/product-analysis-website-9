df8ddac8a64548a42bfa0b87c5f719064e7c40de
AB-Siddique278 <absiddique1060@gmail.com>
2022-04-04 01:19:44 +0600

    Make Home section Done

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Reviews from './Components/Reviews';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header></Header>
      
       <Routes>
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import './Home.css'
+
 
 const Home = () => {
     return (
         <div>
-            <h1>wellcome to home</h1>
+            <div className='container'>
+                <div className='row my-5'>
+
+
+                    <dir className='col-md-6 d-flex align-items-center'>
+                        <div>
+                            <h1 >We are provide best burger</h1>
+                            <p>We offer the best healthy burgers to our customers at the lowest cost</p>
+                            <button type="button" class="btn btn-primary btn-lg">Learn More</button>
+                        </div>
+                    </dir>
+
+
+
+
+                    <div className='col-md-6 product'>
+                        <img src='./images/product.jpg' alt="" />
+                    </div>
+
+
+
+
+
+
+
+
+                </div>
+            </div>
         </div>
     );
 };
